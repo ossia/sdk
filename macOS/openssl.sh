325c4e84cdeba2640da3e81642a3b25f0712ee60
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-11-20 15:41:20 +0100

    add openssl

---
+++ macOS/openssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OPENSSL_VERSION="1_1_0j"
+
+wget -nv https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz 
+gtar xaf OpenSSL_$OPENSSL_VERSION.tar.gz
+
+cd "openssl-OpenSSL_$OPENSSL_VERSION"
+./Configure darwin64-x86_64-cc -static
+make
+
