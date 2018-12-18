ccdd2288703cdea065a6092f5ed7ca645a2fe6ee
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-12-19 00:05:09 +0100

    Various fixes

--- Linux/portaudio.sh
+++ Linux/portaudio.sh
@@ -1,1 +1,26 @@
-../macOS/portaudio.sh
+#!/bin/bash
+
+source ./common.sh
+
+wget -nv http://www.portaudio.com/archives/pa_snapshot.tgz
+tar xaf pa_snapshot.tgz
+
+sed -i '378i TARGET_INCLUDE_DIRECTORIES(portaudio_static PUBLIC "$<INSTALL_INTERFACE:include>")' portaudio/CMakeLists.txt
+sed -i '307d' portaudio/CMakeLists.txt # SET(PA_LIBRARY_DEPENDENCIES ${PA_LIBRARY_DEPENDENCIES} ${ALSA_LIBRARIES})
+sed -i '306d' portaudio/CMakeLists.txt # SET(PA_PKGCONFIG_LDFLAGS "${PA_PKGCONFIG_LDFLAGS} -lasound")
+sed -i '305d' portaudio/CMakeLists.txt
+
+sed -i '305i  SET(PA_PRIVATE_COMPILE_DEFINITIONS ${PA_PRIVATE_COMPILE_DEFINITIONS} PA_USE_ALSA PA_ALSA_DYNAMIC)' portaudio/CMakeLists.txt
+sed -i '305i  SET(PA_PKGCONFIG_LDFLAGS "${PA_PKGCONFIG_LDFLAGS} ${CMAKE_DL_LIBS}")' portaudio/CMakeLists.txt
+      
+      
+cd portaudio/build
+
+cmake .. \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DPA_BUILD_SHARED=Off \
+ -DPA_USE_JACK=Off \
+ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX/portaudio
+
+make -j$NPROC
+make install
--- macOS/portaudio.sh
+++ macOS/portaudio.sh
@@ -5,6 +5,8 @@ source ./common.sh
 wget -nv http://www.portaudio.com/archives/pa_snapshot.tgz
 gtar xaf pa_snapshot.tgz
 
+gsed -i '378i TARGET_INCLUDE_DIRECTORIES(portaudio_static PUBLIC "$<INSTALL_INTERFACE:include>")' portaudio/CMakeLists.txt
+
 cd portaudio/build
 
 cmake .. \
