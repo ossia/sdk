c83c2d821f30a231c04bd110152a314eddf9346f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-01-12 16:28:08 +0100

    add old score sdk files

---
+++ old-sdk/MinGW/qt.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -eux
+
+cd /image
+
+export CC=/usr/local/bin/gcc
+export CXX=/usr/local/bin/g++
+export CFLAGS="-O3"
+export CXXFLAGS="-O3"
+export NPROC=$(nproc)
+
+yum -y install perl harfbuzz-devel which make perl-version libxcb libxcb-devel xcb-util xcb-util-devel fontconfig-devel libX11-devel libXrender-devel libXi-devel git dbus-devel glib2-devel mesa-libGL-devel
+
+git clone https://code.qt.io/qt/qt5.git
+
+(
+  cd qt5
+  git checkout 5.11.2
+  git submodule update --init --recursive qtbase qtdeclarative qtquickcontrols2 qtserialport qtimageformats qtgraphicaleffects qtsvg qtwebsockets
+)
+
+cp /usr/local/bin/* /usr/bin/
+
+mkdir qt5-build
+(
+  cd qt5-build
+  ../qt5/configure -release \
+                   -opensource \
+                   -confirm-license \
+                   -nomake examples \
+                   -nomake tests \
+                   -no-compile-examples \
+                   -no-qml-debug \
+                   -qt-zlib \
+                   -no-mtdev \
+                   -no-journald \
+                   -no-syslog \
+                   -no-gif \
+                   -qt-libpng \
+                   -qt-libjpeg \
+                   -qt-zlib \
+                   -qt-freetype \
+                   -qt-harfbuzz \
+                   -openssl \
+                   -qt-pcre
+                   -no-xinput2 \
+                   -glib \
+                   -no-cups \
+                   -no-iconv \
+                   -no-tslib \
+                   -no-icu \
+                   -no-pch \
+                   -ltcg \
+                   -no-system-proxies \
+                   -xplatform win32-clang-g++ \
+                   -device-option CROSS_COMPILE=/opt/llvm-mingw/bin/x86_64-w64-mingw32-
+
+  gmake -j$NPROC
+  gmake install -j$NPROC
+)
+
