643056c1f4cd3c384ca02329487330a226f9cb59
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-05-26 15:27:28 +0200

    add LLVM files

---
+++ MSYS/llvm-deps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./common.sh
+
+LLVM_VERSION=trunk
+(
+svn co http://llvm.org/svn/llvm-project/llvm/$LLVM_VERSION llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/$LLVM_VERSION clang
+cd ../..
+cd llvm/tools/clang/tools
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/$LLVM_VERSION extra
+cd ../../../..
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/lld/$LLVM_VERSION lld
+cd ../..
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/polly/$LLVM_VERSION polly
+cd ../..
+
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/$LLVM_VERSION compiler-rt
+cd ../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/openmp/$LLVM_VERSION openmp
+cd ../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/libcxx/$LLVM_VERSION libcxx
+cd ../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/libcxxabi/$LLVM_VERSION libcxxabi
+cd ../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/libunwind/$LLVM_VERSION libunwind
+cd ../..
+)
---
+++ MSYS/llvm.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+rm -rf llvm-build
+source ./common.sh
+CMAKE=cmake
+CC=/c/score-sdk/llvm/bin/clang.exe
+CXX=/c/score-sdk/llvm/bin/clang++.exe
+(
+mkdir -p llvm-build
+cd llvm-build
+$CMAKE -G"MSYS Makefiles" \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
+ -DLLVM_TARGETS_TO_BUILD="X86" \
+ -DLLVM_INCLUDE_EXAMPLES=0 \
+ -DLLVM_INCLUDE_TESTS=0 \
+ -DLLVM_ENABLE_CXX1Z=1 \
+ -DCLANG_DEFAULT_CXX_STDLIB:STRING=libc++ \
+ -DCLANG_DEFAULT_RTLIB:STRING=libgcc \
+ -DLIBCXX_ABI_UNSTABLE=ON \
+ -DLIBCXX_USE_COMPILER_RT=OFF \
+ -DLIBCXXABI_USE_COMPILER_RT=OFF \
+ -DLIBUNWIND_USE_COMPILER_RT=OFF \
+ -DCMAKE_INSTALL_PREFIX=$SDK_ROOT/llvm-bootstrap \
+ ../llvm
+
+make -j$NPROC
+make install/strip
+)
+exit 0
+
+# LLVM is bootstrapped so that it is all built with the same libc++ version
+(
+rm -rf llvm-build
+mkdir -p llvm-build
+cd llvm-build
+export PATH=$SDK_ROOT/llvm-bootstrap/bin:$PATH
+export LD_LIBRARY_PATH=$SDK_ROOT/llvm-bootstrap/lib:$LD_LIBRARY_PATH
+
+$CMAKE -G"MinGW Makefiles" \
+ -DCMAKE_C_COMPILER=$SDK_ROOT/llvm-bootstrap/bin/clang \
+ -DCMAKE_CXX_COMPILER=$SDK_ROOT/llvm-bootstrap/bin/clang++ \
+ -DCMAKE_C_FLAGS="-O3" \
+ -DCMAKE_CXX_FLAGS="-O3" \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DBUILD_SHARED_LIBS=0 \
+ -DLLVM_INCLUDE_TOOLS=1 \
+ -DLLVM_BUILD_TOOLS=1 \
+ -DLLVM_INCLUDE_EXAMPLES=0 \
+ -DLLVM_INCLUDE_TESTS=0 \
+ -DLLVM_ENABLE_CXX1Z=1 \
+ -DLLVM_TARGETS_TO_BUILD="X86" \
+ -DLLVM_ENABLE_LIBCXX=ON \
+ -DLLVM_ENABLE_LLD=ON \
+ -DLLVM_ENABLE_EH=ON \
+ -DLLVM_ENABLE_RTTI=ON \
+ -DCLANG_DEFAULT_CXX_STDLIB:STRING=libc++ \
+ -DCLANG_DEFAULT_RTLIB:STRING=libgcc \
+ -DLIBCXX_ABI_UNSTABLE=ON \
+ -DLIBCXX_USE_COMPILER_RT=OFF \
+ -DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON \
+ -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
+ -DLIBCXXABI_ENABLE_STATIC_UNWINDER=ON \
+ -DLIBCXXABI_USE_COMPILER_RT=OFF \
+ -DLIBUNWIND_USE_COMPILER_RT=OFF \
+ -DCOMPILER_RT_USE_BUILTINS_LIBRARY=OFF \
+ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX/llvm \
+ ../llvm
+
+make -j$NPROC
+make install/strip
+)
