5f06b1f9691011a2a7d5ee327b1630530ec2b16b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-12-07 23:13:47 +0100

    devtoolset 8

--- Linux/CentOS/deps-centos.sh
+++ Linux/CentOS/deps-centos.sh
@@ -2,7 +2,7 @@
 
 # For building the toolchain
 yum -y install glibc-devel \
-    devtoolset-7-gcc devtoolset-7-make \
+    devtoolset-8-gcc devtoolset-8-make \
     libxcb-devel xcb-util xcb-util-devel which mesa-libGL-devel \
     rh-git29 svn perl-Data-Dump perl-Data-Dumper \
     cmake3 ncurses-devel zlib-devel
@@ -10,10 +10,10 @@ yum -y install glibc-devel \
 # For building score
 
 yum -y update
-yum -y install epel-release centos-release-scl devtoolset-7
+yum -y install epel-release centos-release-scl devtoolset-8
 yum -y update
 yum -y install glibc-devel \
-    devtoolset-7-gcc devtoolset-7-make \
+    devtoolset-8-gcc devtoolset-8-make \
     libxcb-devel xcb-util xcb-util-devel which mesa-libGL-devel \
     rh-git29 \
     cmake3 ncurses-devel zlib-devel
