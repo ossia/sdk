950d8b744387b5b2d43d9f21f2d6ee0caf78142e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-01-30 15:59:40 +0100

    Add wayland support on linux

--- Linux/qt-deps.sh
+++ Linux/qt-deps.sh
@@ -7,6 +7,7 @@ if [[ ! -d qt5 ]]; then
     cd qt5
     $GIT checkout 5.15
     $GIT submodule update --init --recursive $(cat "$SDK_ROOT/common/qtmodules")
+    $GIT submodule update --init --recursive qtwayland
     $GIT config --global user.email "you@example.com"
     $GIT config --global user.name "Your Name"
 
@@ -14,7 +15,7 @@ if [[ ! -d qt5 ]]; then
     sed -i 's/fuse-ld=gold/fuse-ld=lld/g' \
       mkspecs/common/gcc-base-unix.conf \
       mkspecs/features/qt_configure.prf \
-      configure.json   
+      configure.json
 
     sed -i "s/-O3/$CFLAGS/" mkspecs/common/gcc-base.conf
     sed -i "s/-O2/$CFLAGS/" mkspecs/common/gcc-base.conf
--- Linux/qt.sh
+++ Linux/qt.sh
@@ -4,12 +4,13 @@ source ./common.sh
 mkdir -p qt5-build-static
 (
   cd qt5-build-static
-  
+
   export OPENSSL_LIBS="$INSTALL_PREFIX/openssl/lib/libssl.a $INSTALL_PREFIX/openssl/lib/libcrypto.a -ldl -pthread"
   ../qt5/configure $(cat "$SDK_ROOT/common/qtfeatures") \
                    -static \
                    -system-zlib \
                    -openssl-linked \
+                   -no-feature-wayland-server \
                    -I$INSTALL_PREFIX/openssl/include \
                    -platform linux-clang-libc++ \
                    -prefix $INSTALL_PREFIX/qt5-static
@@ -22,7 +23,7 @@ mkdir -p qt5-build-static
   cd qt5
   $GIT clone https://github.com/jcelerier/qtshadertools.git
   cd qtshadertools
-  $INSTALL_PREFIX/qt5-static/bin/qmake 
+  $INSTALL_PREFIX/qt5-static/bin/qmake
   make -j$NPROC
   make install -j$NPROC
 )
