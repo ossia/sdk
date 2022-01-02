6ff789b875d8656ac90efc466a564708bed8e96a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-01-02 15:04:42 +0100

    add freetype and ysfx

---
+++ MSYS/freetype.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./common.sh
+
+if [[ ! -d freetype ]]; then
+(
+  git clone https://github.com/jcelerier/freetype
+)
+fi
+
+cmake \
+  -G"MSYS Makefiles" \
+  -S freetype \
+  -B freetype-build \
+  -DFT_DISABLE_ZLIB=TRUE \
+  -DFT_DISABLE_BZIP2=TRUE \
+  -DFT_DISABLE_PNG=TRUE \
+  -DFT_DISABLE_HARFBUZZ=TRUE \
+  -DFT_DISABLE_BROTLI=TRUE \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX/freetype
+
+cmake --build freetype-build --parallel
+cmake --build freetype-build --target install/strip
---
+++ MSYS/ysfx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./common.sh
+
+if [[ ! -d ysfx ]]; then
+(
+  git clone --recursive https://github.com/jcelerier/ysfx
+)
+fi
+
+export FREETYPE_DIR=$INSTALL_PREFIX/freetype
+
+cmake \
+  -G"MSYS Makefiles" \
+  -S ysfx \
+  -B ysfx-build \
+  -DYSFX_PLUGIN=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+  -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX/ysfx 
+
+cmake --build ysfx-build --parallel
+cmake --build ysfx-build --target install/strip
