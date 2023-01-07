250548d26b30d33756599a56d51b82eb1882d1d6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-01-07 17:28:58 -0500

    ffmpeg hwaccel on macos

--- macOS/ffmpeg.sh
+++ macOS/ffmpeg.sh
@@ -3,7 +3,7 @@
 source ./common.sh
 VERSION=snapshot
 
-if [[ ! -d ffmpeg-$VERSION ]]; then
+if [[ ! -d ffmpeg ]]; then
   wget -nv https://ffmpeg.org/releases/ffmpeg-$VERSION.tar.bz2
   gtar xaf ffmpeg-$VERSION.tar.bz2
 fi
@@ -15,13 +15,15 @@ xcrun ../ffmpeg/configure \
     --arch=x86_64 --cpu=x86_64 \
  	--disable-doc --disable-ffmpeg --disable-ffplay \
  	--disable-debug \
+	--disable-autodetect \
+	--enable-videotoolbox --enable-audiotoolbox \
  	--pkg-config-flags="--static" \
  	--enable-gpl --enable-version3 \
  	--disable-openssl --disable-securetransport \
  	--disable-network --disable-iconv --disable-libxcb \
  	--enable-protocols --disable-lzma \
  	--prefix=$INSTALL_PREFIX/ffmpeg \
-        --cc="$CC" --cxx="$CXX" \
+    --cc="$CC" --cxx="$CXX" \
  	--extra-cflags="$CFLAGS"
 
 xcrun make -j$NPROC
