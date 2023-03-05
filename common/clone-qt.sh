eca120de999ee53589dd9a4f9aece9afc25f716a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-03-05 18:18:48 -0500

    macos: migrate to qt6

---
+++ common/clone-qt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ ! -d qt ]]; then
+git clone https://github.com/qt/qt5 qt -b 6.5
+
+(
+  cd qt
+  git submodule update --init --recursive $(cat "$SDK_COMMON_ROOT/common/qtmodules")
+
+  (
+    cd qtbase
+    git remote add jcelerier https://github.com/jcelerier/qtbase
+    git fetch jcelerier
+    git checkout jcelerier/6.5-ossia
+
+    git fetch https://codereview.qt-project.org/qt/qtbase refs/changes/62/464662/1 && git cherry-pick FETCH_HEAD
+  )
+
+  (
+    cd qtdeclarative
+    git fetch https://codereview.qt-project.org/qt/qtdeclarative refs/changes/68/464668/1 && git cherry-pick FETCH_HEAD
+  )
+
+  (
+    cd qtshadertools
+    git fetch https://codereview.qt-project.org/a/qt/qtshadertools refs/changes/63/464663/2 && git cherry-pick FETCH_HEAD
+  )
+)
+fi
--- macOS/qt.sh
+++ macOS/qt.sh
@@ -3,63 +3,33 @@
 export SDK_COMMON_ROOT=$(cd "$PWD/.." ; pwd -P)
 source ./common.sh
 
-if [[ ! -d qt5 ]]; then
-git clone https://invent.kde.org/qt/qt/qt5
+source "$SDK_COMMON_ROOT/common/clone-qt.sh"
 
-(
-  cd qt5
-  git checkout kde/5.15
-  git submodule update --init --recursive $(cat "$SDK_COMMON_ROOT/common/qtmodules")
-
-  (
-    cd qtbase
-    git remote add jcelerier https://github.com/jcelerier/qtbase
-    git fetch jcelerier
-    git checkout jcelerier/kde/5.15
-  )
-  
-  git clone https://github.com/jcelerier/qtshadertools.git
-)
 
 # disabled since we can't seem to make custom libc++ not crash...
 # echo 'QMAKE_LFLAGS+= -L/opt/score-sdk-osx/llvm/lib -lc++ -lc++abi -Wl,-rpath,/opt/score-sdk-osx/llvm/lib' >> qt5/qtbase/mkspecs/common/clang-mac.conf
 
+mkdir -p qt6-build-static
 (
-  cd qt5/qtbase/mkspecs/common
-  gsed -i "s/-O2/$CFLAGS/" gcc-base.conf
-  gsed -i 's/-fvisibility=hidden/-fvisibility=default/g' gcc-base.conf
-  gsed -i 's/-fvisibility-inlines-hidden/ /g' gcc-base.conf
-
-  # gsed -i "s/10.13/10.14/" macx.conf
-  #echo "QMAKE_CFLAGS+=$CFLAGS" >> clang-mac.conf
-  #echo "QMAKE_CXXFLAGS+=$CXXFLAGS" >> clang-mac.conf
-)
-fi
-
-
-mkdir -p qt5-build-static
-(
-  cd qt5-build-static
-  ../qt5/configure $(cat "$SDK_COMMON_ROOT/common/qtfeatures") \
+  cd qt6-build-static
+  ../qt/configure $(cat "$SDK_COMMON_ROOT/common/qtfeatures") \
                    -static \
-                   -system-zlib -no-feature-zstd \
+                   -system-zlib \
                    -system-freetype \
-                   FREETYPE_INCDIR=$INSTALL_PREFIX/freetype/include/freetype2 \
-                   FREETYPE_LIBS=$INSTALL_PREFIX/freetype/lib/libfreetype.a \
                    -system-harfbuzz \
-                   HARFBUZZ_INCDIR=$INSTALL_PREFIX/harfbuzz/include \
-                   HARFBUZZ_LIBS=$INSTALL_PREFIX/harfbuzz/lib/libharfbuzz.a \
-                   -prefix $INSTALL_PREFIX/qt5-static
+                   -prefix $INSTALL_PREFIX/qt6-static \
+                   -c++std c++20 \
+                   -DYYDEBUG=0 \
+                   -- \
+                   -DCMAKE_C_FLAGS="$CFLAGS" \
+                   -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+                   -DCMAKE_CXX_STANDARD=20 \
+                   -DCMAKE_PREFIX_PATH=$INSTALL_PREFIX \
+                   -Dharfbuzz_DIR=$INSTALL_PREFIX/harfbuzz \
+                   -DHARFBUZZ_INCLUDE_DIRS=$INSTALL_PREFIX/harfbuzz/include \
+                   -DHARFBUZZ_LIBRARIES=$INSTALL_PREFIX/harfbuzz/lib/libharfbuzz.a
 
-  make -j$NPROC
-  make install -j$NPROC
+  
+  cmake --build .
+  cmake --build . --target install
 )
-(
-  cd qt5
-  cd qtshadertools
-  git clean -dffx
-  $INSTALL_PREFIX/qt5-static/bin/qmake 
-  make -j$NPROC
-  make install -j$NPROC
-)
-
