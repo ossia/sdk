798bd886114c65033dcf28d7501dc37051b7d39c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-04-11 19:40:21 -0400

    qt: gles on aarch64

--- Linux/qt.sh
+++ Linux/qt.sh
@@ -4,6 +4,16 @@ source ./common.sh clang
 
 source "$SDK_COMMON_ROOT/common/clone-qt.sh"
 
+declare -a QT_AARCH64_FLAGS=(
+  -opengl es2
+  -opengles3
+)
+
+declare -a QT_X86_64_FLAGS=(
+)
+
+declare -n QT_ARCH_FLAGS=QT_${ARCH_VARNAME}_FLAGS
+
 mkdir -p qt6-build-static
 (
   cd qt6-build-static
@@ -26,6 +36,7 @@ mkdir -p qt6-build-static
   -platform linux-clang-libc++ \
   -prefix $INSTALL_PREFIX/qt6-static \
   -openssl-linked \
+  "${QT_ARCH_FLAGS[@]}" \
   -- \
   -DCMAKE_C_FLAGS="$CFLAGS" \
   -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
@@ -35,7 +46,8 @@ mkdir -p qt6-build-static
   -Dharfbuzz_DIR="$INSTALL_PREFIX/sysroot" \
   -DHARFBUZZ_INCLUDE_DIRS="$INSTALL_PREFIX/sysroot/include" \
   -DHARFBUZZ_LIBRARIES="$INSTALL_PREFIX/sysroot/lib/libharfbuzz.a" \
-  -DOPENSSL_ROOT_DIR="$INSTALL_PREFIX/openssl"
+  -DOPENSSL_ROOT_DIR="$INSTALL_PREFIX/openssl" \
+  -DQT_DISABLE_DEPRECATED_UP_TO=0x060900
   
   cmake --build .
   cmake --build . --target install
