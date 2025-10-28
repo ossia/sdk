eff54328ccd7379476991a6318c8c0272d4651f9
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2025-10-28 03:29:52 -0400

    ffmpeg and qt fixes

--- MSYS/ffmpeg.sh
+++ MSYS/ffmpeg.sh
@@ -8,20 +8,20 @@ cd ffmpeg-$FFMPEG_VERSION
 
 export PKG_CONFIG_PATH="$INSTALL_PREFIX/sysroot/lib/pkgconfig"
 export PKG_CONFIG_LIBDIR="$PKG_CONFIG_PATH"
-export CLFAGS="-isystem $INSTALL_PREFIX/sysroot/include $CFLAGS"
+export CFLAGS="-isystem $INSTALL_PREFIX/sysroot/include $CFLAGS -march=x86-64-v3"
 export LDFLAGS="-L$INSTALL_PREFIX/sysroot/lib $LDFLAGS"
 ./configure \
-    --arch=x86-64-v3  \
  	--disable-doc --disable-ffmpeg --disable-ffplay \
  	--disable-debug \
-	--enable-dxva2 --enable-d3d11va \
+	--enable-dxva2 \
+	--enable-d3d11va \
  	--pkg-config-flags="--static" \
     --pkg-config="pkg-config" \
  	--enable-gpl --enable-version3 \
  	--disable-openssl --disable-securetransport \
  	--disable-videotoolbox \
 	--enable-libsnappy \
- 	--disable-network --disable-iconv \
+ 	--disable-network --disable-iconv  --disable-response-files \
  	--enable-protocols \
 	--extra-cflags=" $CFLAGS " \
 	--extra-ldflags=" $LDFLAGS " \
