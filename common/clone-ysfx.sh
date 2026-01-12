d0bd0c8e4c1c4336dcec5988368e7e0f68c35e0f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-01-11 23:59:53 -0500

    ysfx: use proper branch

--- common/clone-ysfx.sh
+++ common/clone-ysfx.sh
@@ -4,8 +4,6 @@ source ../common/versions.sh
 
 if [[ ! -d ysfx ]]; then
 (
-  git clone --recursive https://github.com/jcelerier/ysfx
-  cd ysfx
-  git checkout fae7420f6bf10fbe8cd22da6d1974af755514b45
+  git clone --recursive https://github.com/jcelerier/ysfx -b ossia/2026-01
 )
 fi
