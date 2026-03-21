4e77ab14c0ebd667f3085f3193834b995e5aa5af
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-03-21 17:21:32 +0100

    ffmpeg: linux 8.1 patch

--- common/clone-ffmpeg.sh
+++ common/clone-ffmpeg.sh
@@ -19,9 +19,7 @@ if [[ ! -d ffmpeg-$FFMPEG_VERSION ]]; then
      ( 
       yum -y install patch
       cd ffmpeg-$FFMPEG_VERSION
-      curl -ksSLOJ https://raw.githubusercontent.com/HiassofT/LibreELEC.tv/refs/heads/le13-ffmpeg-8.0/packages/multimedia/ffmpeg/patches/rpi/ffmpeg-001-rpi.patch
-      ls 
-      patch -p1 < ffmpeg-001-rpi.patch
+      patch -p1 < ../../common/patches/ffmpeg/0001-rpi.patch
 
       sed -i "1i \ #define V4L2_PIX_FMT_P010    v4l2_fourcc('P', '0', '1', '0') /* 24  Y/CbCr 4:2:0 10-bit per component */"  libavcodec/v4l2_req_hevc_vx.c
      )
